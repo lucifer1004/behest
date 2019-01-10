afa8bb8819e1039b282ade9321c7ee37bbf9f7b8
Gabriel Wu <qqbbnease1004@126.com>
2019-01-10 15:49:00 +0800

    feat: basic structure for a TS project

---
+++ src/helpers/BooksAPI.ts
@@ -0,0 +1,49 @@
+const BASE_URI = 'https://reactnd-books-api.udacity.com'
+
+interface Book {
+  id: Number
+}
+
+// Generate a unique token for storing your bookshelf data on the backend server.
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random()
+    .toString(36)
+    .substr(-8)
+
+const headers = {
+  Accept: 'application/json',
+  Authorization: token,
+}
+
+export const get = (bookId: Number) =>
+  fetch(`${BASE_URI}/books/${bookId}`, {headers})
+    .then(res => res.json())
+    .then(data => data.book)
+
+export const getAll = () =>
+  fetch(`${BASE_URI}/books`, {headers})
+    .then(res => res.json())
+    .then(data => data.books)
+
+export const update = (book: Book, shelf: string) =>
+  fetch(`${BASE_URI}/books/${book.id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({shelf}),
+  }).then(res => res.json())
+
+export const search = (query: string) =>
+  fetch(`${BASE_URI}/search`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({query}),
+  })
+    .then(res => res.json())
+    .then(data => data.books)
