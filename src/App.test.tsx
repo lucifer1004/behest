32286cef3cc1457a2d97f56ab4593f79c73ed35f
Gabriel Wu <qqbbnease1004@126.com>
2019-01-11 00:04:50 +0800

    test: snapshot testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
   ReactDOM.unmountComponentAtNode(div)
+})
+
+it('snapshot app', () => {
+  const app = renderer.create(<App />)
+  let tree = app.toJSON()
+  expect(tree).toMatchSnapshot()
 })
