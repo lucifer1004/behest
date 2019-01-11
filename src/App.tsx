5e6268e8eb2d7d39069ee25418bf8ab1e45c82bc
Gabriel Wu <qqbbnease1004@126.com>
2019-01-11 14:45:05 +0800

    refactor: refactor BooksApp to a functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'
 import {AppState} from './common/types'
 import {get, getAll, update, search} from './helpers/BooksAPI'
@@ -37,62 +37,49 @@ const Main: React.FunctionComponent<AppState> = ({
   </div>
 )
 
-class BooksApp extends React.Component<{}, AppState> {
-  constructor(props: any) {
-    super(props)
-    this.state = {
-      isLoading: false,
-      newBooks: [],
-      booksToRead: [],
-      booksRead: [],
-      booksReading: [],
-    }
-  }
+const BooksApp: React.FunctionComponent<{}> = () => {
+  const [loadingStatus, setLoadingStatus] = useState(false)
+  const [newBooks, setNewBooks] = useState([])
+  const [booksToRead, setBooksToRead] = useState([])
+  useEffect(() => {
+    getAll()
+      .then(books => setNewBooks(books))
+      .then(() => setLoadingStatus(true))
+  })
 
-  componentWillMount() {
-    /**
-     * Fetch new books
-     */
-    getAll().then(books => {
-      this.setState(
-        {
-          newBooks: books,
-        },
-        () => {
-          this.setState({
-            isLoading: true,
-          })
-        },
-      )
-    })
-
+  useEffect(() => {
     const localBooks = localStorage.getItem('local-books')
     if (!localBooks) {
       search('Android').then(books => {
-        this.setState({
-          booksToRead: books,
-        })
+        setBooksToRead(books)
         localStorage.setItem('local-books', JSON.stringify(books))
       })
     } else {
-      this.setState({
-        booksToRead: JSON.parse(localBooks),
-      })
+      setBooksToRead(JSON.parse(localBooks))
     }
-  }
+  })
 
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="app">
-          <Switch>
-            <Route path="/search" component={SearchBox} />
-            <Route path="/" render={() => <Main {...this.state} />} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    )
-  }
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+          <Route path="/search" component={SearchBox} />
+          <Route
+            path="/"
+            render={() => (
+              <Main
+                isLoading={loadingStatus}
+                newBooks={newBooks}
+                booksToRead={booksToRead}
+                booksRead={[]}
+                booksReading={[]}
+              />
+            )}
+          />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default BooksApp
