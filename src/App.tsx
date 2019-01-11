7f92a92822a4f3a7fb5a91a92182fa81d7e8df37
Gabriel Wu <qqbbnease1004@126.com>
2019-01-11 12:26:44 +0800

    refactor: router, switch, route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
 import React from 'react'
-import {BrowserRouter, Link} from 'react-router-dom'
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'
 import {AppState} from './common/types'
 import {get, getAll, update, search} from './helpers/BooksAPI'
 import Shelf from './components/Shelf'
+import SearchBox from './components/SearchBox'
 import './App.css'
 
-class BooksApp extends React.PureComponent<{}, AppState> {
+const Main: React.FunctionComponent<AppState> = ({
+  isLoading,
+  newBooks,
+  booksToRead,
+  booksReading,
+  booksRead,
+}) => (
+  <div className="list-books">
+    <div className="list-books-title">
+      <h1>MyReads</h1>
+    </div>
+    <div className="list-books-content">
+      <div>
+        {isLoading ? (
+          <Shelf books={newBooks} title="New books" />
+        ) : (
+          <Shelf books={[]} title="Loading new books..." />
+        )}
+      </div>
+      <Shelf books={booksToRead} title="Want to read" />
+      <Shelf books={booksReading} title="Current reading" />
+      <Shelf books={booksRead} title="Read" />
+    </div>
+    <div className="open-search">
+      <Link to="/search">
+        <button>Add a book</button>
+      </Link>
+    </div>
+  </div>
+)
+
+class BooksApp extends React.Component<{}, AppState> {
   constructor(props: any) {
     super(props)
     this.state = {
@@ -50,52 +82,15 @@ class BooksApp extends React.PureComponent<{}, AppState> {
   }
 
   render() {
-    console.log(location.href.split('/').reverse()[0])
     return (
-      <div className="app">
-        {location.href.split('/').reverse()[0] === 'search' ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <BrowserRouter>
-                <Link to="/">
-                  <button className="close-search">Close</button>
-                </Link>
-              </BrowserRouter>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author" />
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid" />
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                {this.state.isLoading ? (
-                  <Shelf books={this.state.newBooks} title="New books" />
-                ) : (
-                  <Shelf books={[]} title="Loading new books..." />
-                )}
-              </div>
-              <Shelf books={this.state.booksToRead} title="Want to read" />
-              <Shelf books={this.state.booksReading} title="Current reading" />
-              <Shelf books={this.state.booksRead} title="Read" />
-            </div>
-            <BrowserRouter>
-              <div className="open-search">
-                <Link to="/search">
-                  <button>Add a book</button>
-                </Link>
-              </div>
-            </BrowserRouter>
-          </div>
-        )}
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <Switch>
+            <Route path="/search" component={SearchBox} />
+            <Route path="/" render={() => <Main {...this.state} />} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     )
   }
 }
---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const SearchBox: React.FunctionComponent = ({}) => (
+  <div className="search-books">
+    <div className="search-books-bar">
+      <Link to="/">
+        <button className="close-search">Close</button>
+      </Link>
+      <div className="search-books-input-wrapper">
+        <input type="text" placeholder="Search by title or author" />
+      </div>
+    </div>
+    <div className="search-books-results">
+      <ol className="books-grid" />
+    </div>
+  </div>
+)
+
+export default SearchBox
